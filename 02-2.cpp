131f0dc3acd5008b0d7f65f40bb78aa68032b8a5
Abdulrhman Farrah <abdulrhman.mi.farrah@gmail.com>
2024-10-23 17:41:14 +0300

    Update 02-2.cpp

--- 02-2.cpp
+++ 02-2.cpp
@@ -1,9 +1,64 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+int ReadPositiveNumber(string message) // let the user enter a positive integer number 
+{
+	int num;
+	cout << message << endl;
+	cin >> num;
+
+	while (num < 1)
+	{
+		cout << "Wrong it should be greater or equal to one: ";
+		cin >> num;
+	}
+
+	return num;
+}
+									 
+bool CheckDivisionOdd(int num)	 
+{								
+	for (int i = 3; i <= floor(sqrt(num)); i += 2)
+	{
+		if (num % i == 0)
+			return true;
+	}
+	return false;
+}
+
+bool CheckPrime(int num)  // Check the number if it is prime or not then return true or false
+{
 
 
-// c++ course solution: 
+	if (num == 2)
+		return true;
+	else if (num % 2 == 0)
+		return false;
+	else if (CheckDivisionOdd(num))
+		return false;
+	else
+		return true;
+}
+
+int main()
+{
+
+	while (true) {
+
+		int number = ReadPositiveNumber("Enter a number:");
+		system("cls");
+		if (CheckPrime(number))
+			cout << number << " is a prime number" << endl;
+		else
+			cout << number << " is not a prime number" << endl;
+	}
+
+	return 0;
+}
+
+										// another solution: 
+/*
 
 enum enType { Prime, NotPrime };
 
@@ -45,7 +100,6 @@ enType CheckPrime(int N)
 
 void PrintAllPrimeNumbers(int Number)
 {
-
 	for (int i = 1; i <= Number; i++)
 	{
 		if (CheckPrime(i) == enType::Prime)
@@ -53,29 +107,19 @@ void PrintAllPrimeNumbers(int Number)
 			cout << i << endl;
 		}
 	}
-
-
-
 }
 
 int main()
 {
-
 	int number = ReadPositiveNumber("Enter a number:");
 
 	cout << endl << "***********************************" << endl;
 
 	PrintAllPrimeNumbers(number);
-
-
 	return 0;
 }
-
-
-
-
-
-		// My solution: 
+*/
+							// My first solution: 
 /*
 
  // ---------------------------------------------------------------------------------------------------------
@@ -85,19 +129,15 @@ enum enType { Prime, NotPrime };
 
 int ReadPositiveNumber(string message)
 {
-
 	int num;
 	cout << message << endl;
 	cin >> num;
-
-
+ 
 	while (num <= 0)
 	{
 		cout << "Wrong number, the number should be positive: " << endl;
 		cin >> num;
 	}
-	
-
 	return num;
 }
 
@@ -129,7 +169,6 @@ enType CheckIfPrime(int N)
 
 void PrintAllPrimeNumbers(int Number)
 {
-
 	while (Number > 1)
 	{
 
@@ -146,74 +185,13 @@ void PrintAllPrimeNumbers(int Number)
 
 int main()
 {
-
 	int Number = ReadPositiveNumber("Enter Number: ");
 	
 	cout <<  endl << "----------------------------" << endl;
 
 	PrintAllPrimeNumbers(Number);
 
-
-
 	return 0;
 }
  // ---------------------------------------------------------------------------------------------------------
-*/
-
-
-	// discrete math course solution: 
-
-
-/*
-
- // ---------------------------------------------------------------------------------------------------------
-
-int ReadPositiveNumber()
-{
-
-	int num;
-	cout << "Enter a positive number: ";
-	cin >> num;
-
-	while (num <= 0)
-	{
-		cout << "Wrong number, it should be positive: ";
-		cin >> num;
-	}
-
-	return num;
-
-}
-
-bool CheckPrime(int N, int primeNumbers[78])
-{
-	if (N == 2)
-		return true;
-	else if (N % 2 == 0)
-		return false;
-
-	int K = floor(sqrt(N));
-	int location_in_array = 0;
-
-	for (int i = 0; primeNumbers[i] <= K; i++)
-	{
-		if (primeNumbers[i] > K)
-			location_in_array = i - 1;
-	}
-
-
-}
-
-int main()
-{
-	int N = ReadPositiveNumber();
-	int most_of_prime_numbers[78] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199 , 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293 , 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397 };
-	cout << endl << "-----------------------" << endl;
-
-
-	return 0;
-}
-
- // ---------------------------------------------------------------------------------------------------------
-
 */
